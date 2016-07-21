8d6657ffc4dd3d8fc442bb21468e8d86b43cfeeb
Ariana Barbera <abb44@cam.ac.uk>
2016-07-21 16:52:42 +0100

    UPDATE of README file for contributions

---
+++ inst/scripts/model_organism_distribution.R
@@ -0,0 +1,5 @@
+library(ddiR)
+library(myTAI)
+
+model_organism <- read.table(file="inst/data/model_organism.tsv", sep = "/t", header = TRUE)
+
--- inst/scripts/query_all_datasets.R
+++ inst/scripts/query_all_datasets.R
@@ -14,9 +14,16 @@ for(datasetCount in seq(from = 0, to = datasets@count, by = 100)){
   }
 }
 
-ggplot(d, aes(factor(d$DatasetDetail.database), fill = factor(d$DatasetDetail.database))) +
-  geom_bar() + scale_y_sqrt() + labs(title = "Number of Omics Datasest by Respoitory", x = "Repositories/Databases", y = "Number of Datasests (sqrt scale)") +
+save(d, file="inst/data/datasets-d.RData")
+
+plot <- ggplot(d, aes(factor(d$DatasetDetail.database), fill = factor(d$DatasetDetail.database))) +
+  geom_bar() + scale_y_sqrt(breaks = c(100, 1000, 4000, 10000, 20000, 40000, 65000)) + labs(title = "Number of Omics Datasests by Respoitory", x = "Repositories/Databases", y = "Number of Datasests (sqrt scale)") +
   scale_fill_discrete(guide = guide_legend(NULL), labels = c("ArrayExpress", "ExpressionAtlas", "EGA", "GNPS", "MassIVE", "Metabolights", "MetabolomeExpress", "MetabolomicsWorkbench", "PeptideAtlas", "PRIDE")) + 
   theme(axis.ticks = element_blank(), 
         axis.text.x = element_blank(), panel.background = element_blank())
+
+png(file = "inst/imgs/datasets-by-repository.png", width = 800, height = 600)
+plot(plot)
+dev.off()
+
 
