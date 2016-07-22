4c030500e57c6bdf9e52f68dc33265164865fe26
Ariana Barbera <abb44@cam.ac.uk>
2016-07-22 09:15:45 +0100

    Bug fixed with the null omicsdi links

--- inst/scripts/query_all_datasets.R
+++ inst/scripts/query_all_datasets.R
@@ -22,7 +22,7 @@ save(d, file="inst/data/datasets-d.RData")
 
 plot <- ggplot(d, aes(factor(d$DatasetDetail.database), fill = factor(d$DatasetDetail.database))) +
   geom_bar() + scale_y_sqrt(breaks = c(100, 1000, 4000, 10000, 20000, 40000, 65000)) + labs(title = "Number of Omics Datasests by Respoitory", x = "Repositories/Databases", y = "Number of Datasests (sqrt scale)") +
-  scale_fill_discrete(guide = guide_legend(NULL), labels = c("ArrayExpress", "ExpressionAtlas", "EGA", "GNPS", "MassIVE", "Metabolights", "MetabolomeExpress", "MetabolomicsWorkbench", "PeptideAtlas", "PRIDE")) + 
+  scale_fill_discrete(guide = guide_legend(NULL), labels = c("ArrayExpress", "ExpressionAtlas", "EGA", "GNPS", "GPMDB", "MassIVE", "Metabolights", "MetabolomeExpress", "MetabolomicsWorkbench", "PeptideAtlas", "PRIDE")) + 
   theme(axis.ticks = element_blank(), 
         axis.text.x = element_blank(), panel.background = element_blank())
 
