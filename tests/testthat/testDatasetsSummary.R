1aab6b6a350f1d2cb9e15ca08bc95587e9573fbc
enriquea <enrique.audain@gmail.com>
2020-03-16 18:54:39 +0100

    added tests

---
+++ tests/testthat/testDatasetsSummary.R
@@ -0,0 +1,28 @@
+library(ddiR)
+context("Datasets Summary")
+
+# Getting all Datasets with 'NOTCH1' in its meta-data
+datasets <- search.DatasetsSummary(query = "NOTCH1")
+
+test_that("count attribute returns found all NOTCH1 datsets", {
+    expect_gt(datasets@count, 867)
+})
+
+# Getting 10 datasets with 'NOTCH1' in its meta-data and check
+datasets <- search.DatasetsSummary(query = "NOTCH1", size = 10)
+
+match_query = vector()
+
+for(dataset in datasets@datasets){
+    dataset = get.DatasetDetail(accession=dataset.id(dataset),
+                                database=database(dataset))
+    match_query[dataset.id(dataset)] <- any(grepl('notch1',
+                                          tolower(c(get.description(dataset),
+                                          get.dataset.name(dataset)))))
+
+}
+
+test_that("check match of 'NOTCH1' in the meta-data of all 10 dataset", {
+    expect_true(all(match_query))
+})
+
